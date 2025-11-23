ec49742b8fc0c8043868f7bbbda5a6d2c34b3cac
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-11-23 17:32:04 +0100

    feat: Working driver for AN196
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 
 const Homey = require('homey');
 
-module.exports = class MyApp extends Homey.App {
+module.exports = class NexaZwaveDevicesApp extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('NexaZwaveDevicesApp has been initialized');
   }
-
 };
